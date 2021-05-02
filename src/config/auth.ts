c67bf183e1f88df3643c809db22b562ac3f13c73
Filipe Souza <filipe.souza1602@gmail.com>
2021-05-02 12:37:20 -0300

    upload of api-vendas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '91a9ea50c12cc0fa483e107281a38d23',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import { isAfter, addHours } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UserTokenRepository from '../typeorm/repositories/UserTokenRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UserTokenRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User Token does not exists.');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const comparaDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), comparaDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
